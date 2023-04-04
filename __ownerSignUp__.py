2f5fd9a9d4765f124c50213d13ae9c849cdfd8d6
Isabelle Hondo <isefshondo@gmail.com>
2023-04-04 10:35:21 -0300

    Feat: Showing header elements

---
+++ __ownerSignUp__.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+ownerInterface = Tk()
+
+# Screen Configuration
+ownerInterface.resizable(False, False)
+
+ownerInterface.state("zoomed")
+
+ownerInterface.title("Petshop Dogin's")
+ownerInterface.configure(bg = "#FFF")
+
+
+# Get Screen Measurements to Responsivity
+taskBarHeight = 40
+
+width_screen = ownerInterface.winfo_screenwidth()
+height_screen = ownerInterface.winfo_screenheight() - taskBarHeight
+
+# Styling Owner's Interface
+logoImage = PhotoImage(file = r"assets\imgs\secondary-logo.png")
+logoHolder = Label(ownerInterface, bg = "#FFF", image = logoImage)
+logoHolder.grid(column = 0, row = 0)
+
+progressImage = PhotoImage(file = r"assets\imgs\progress-bar\initial-progress.png")
+progressHolder = Label(ownerInterface, bg = "#FFF", image = progressImage)
+progressHolder.grid(column = 1, row = 0)
+
+goBackButton = Button(ownerInterface, text = "Voltar ao menu", bd = 0, bg = "#FFF", fg = "#777777", font = "Helvetica 10 underline", activebackground = "#FFF", activeforeground = "#777")
+goBackButton.grid(column = 2, row = 0)
+
+ownerInterface.mainloop()

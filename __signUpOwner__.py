e2418e25710ee8f4909fe9800e6c09c9c625b0d3
3º DSM <dsm-3@fatec.academico>
2023-04-05 20:49:11 -0300

    Chore: Show left border in TextBoxes is on process

--- __signUpOwner__.py
+++ __signUpOwner__.py
@@ -19,33 +19,33 @@ height_screen = signUpOwner.winfo_screenheight()
 # Header of the Sign Up Owner Interface
 mainLogoImage = PhotoImage(file = r"images\mainLogo.png")
 mainLogoHolder = Label(signUpOwner, image = mainLogoImage, bg = "#FFF")
-mainLogoHolder.grid(column = 0, row = 0, sticky = "nw")
+mainLogoHolder.grid(column = 0, row = 0, sticky = "nw", padx = 5)
 
 progressBarImage = PhotoImage(file = r"images\progressBar.png")
 progressBarHolder = Label(signUpOwner, image = progressBarImage, bg = "#FFF")
 progressBarHolder.grid(column = 1, row = 0, sticky = "nsew")
 
-goBackButton = Button(signUpOwner, text = "Voltar ao menu", activebackground = "#FFF", activeforeground = "#777", bg = "#FFF", fg = "#777", bd = 0)
-goBackButton.grid(column = 2, row = 0, sticky = "nsew")
+goBackButton = Button(signUpOwner, text = "Voltar ao menu", activebackground = "#FFF", activeforeground = "#777", bg = "#FFF", fg = "#777", font = ("Open Sans", 18, "underline"), bd = 0)
+goBackButton.grid(column = 2, row = 0, sticky = "nse", padx = 5)
 # End of the Sign Up's Page Header
 
-mainElementsWrapper = Frame(signUpOwner, bd = 2, bg = "#FF0000", relief = "flat")
+mainElementsWrapper = Frame(signUpOwner, bd = 2, bg = "#D9D9D9", relief = "flat")
 mainElementsWrapper.grid(column = 0, row = 1, sticky = "nsew", columnspan = 3, padx = 20, pady = 20)
 mainElementsWrapper.grid_columnconfigure(0, weight = 1)
 mainElementsWrapper.grid_columnconfigure(1, weight = 2)
 mainElementsWrapper.grid_columnconfigure(2, weight = 1)
 mainElementsWrapper.grid_rowconfigure(1, weight = 1)
 
-interfaceDescription = Label(mainElementsWrapper, text = "Cadastro do Cliente", font = ("Open Sans", 35, "bold"))
+interfaceDescription = Label(mainElementsWrapper, text = "Cadastro do Cliente", bg = "#FFF", font = ("Open Sans", 35, "bold"))
 interfaceDescription.grid(column = 0, row = 0, sticky = "we", columnspan = 3)
 
-uploadImageFrame = Frame(mainElementsWrapper, bg = "#f0f0f0")
+uploadImageFrame = Frame(mainElementsWrapper, bg = "#FFF")
 uploadImageFrame.grid(column = 0, row = 1, sticky = "nsew")
 
-aTestingFrame = Frame(mainElementsWrapper, bg = "#1D5DEC")
+aTestingFrame = Frame(mainElementsWrapper, bg = "#FFF")
 aTestingFrame.grid(column = 1, row = 1, sticky = "nsew")
 
-signUpFrame = Frame(aTestingFrame, bg = "#ff0000")
+signUpFrame = Frame(aTestingFrame, bg = "#FFF")
 signUpFrame.place(relx = .5, rely = .35, anchor = "center")
 
 signUpFrame.grid_columnconfigure(0, weight = 1)
@@ -67,34 +67,44 @@ defaultProfilePhoto = ImageTk.PhotoImage(defaultPhotoSize)
 defaultUploadLabel = Label(uploadImageFrame, image = defaultProfilePhoto)
 defaultUploadLabel.place(relx = .5, rely = .3, anchor = "center")
 
-uploadButton = Button(uploadImageFrame, text = "Upload de Imagem", bd = 0, bg = "#FFF", padx = 25, pady = 5)
+uploadButton = Button(uploadImageFrame, text = "Upload de Imagem", bd = 0, bg = "#D5D5D5", padx = 25, pady = 5)
 uploadButton.place(relx = .5, rely = .55, anchor = "center")
 
 # Sign Up
-idClient = Label(signUpFrame, text = "Código do cliente")
+idClient = Label(signUpFrame, text = "Código do cliente", bg = "#FFF")
 idClient.grid(column = 0, row = 0, sticky = "w")
-idClientEntry = Entry(signUpFrame)
-idClientEntry.grid(column = 0, row = 1, sticky = "we")
+borderEntryId = Frame(signUpFrame, bg = "#85D3FF")
+borderEntryId.grid(column = 0, row = 1, sticky = "we")
+idClientEntry = Entry(borderEntryId)
+idClientEntry.grid(column = 0, row = 0, padx = (1.5, 0))
 
 nCpfClient = Label(signUpFrame, text = "CPF")
 nCpfClient.grid(column = 0, row = 3, sticky = "w")
-nCpfClientEntry = Entry(signUpFrame)
-nCpfClientEntry.grid(column = 0, row = 4, sticky = "nwn")
+borderEntryCpf = Frame(signUpFrame, bg = "#85D3FF")
+borderEntryCpf.grid(column = 0, row = 4, sticky = "nwn")
+nCpfClientEntry = Entry(borderEntryCpf)
+nCpfClientEntry.grid(column = 0, row = 0, padx = (1.5, 0))
 
 nTelClient = Label(signUpFrame, text = "Telefone residencial")
 nTelClient.grid(column = 0, row = 6, sticky = "w")
-nTelClientEntry = Entry(signUpFrame)
-nTelClientEntry.grid(column = 0, row = 7, sticky = "nwn")
+borderEntryNTel = Frame(signUpFrame, bg = "#85D3FF")
+borderEntryNTel.grid(column = 0, row = 7, sticky = "nwn")
+nTelClientEntry = Entry(borderEntryNTel)
+nTelClientEntry.grid(column = 0, row = 0, padx = (1.5, 0))
 
 nCepClient = Label(signUpFrame, text = "CEP")
 nCepClient.grid(column = 0, row = 9, sticky = "w")
-nCepClientEntry = Entry(signUpFrame)
-nCepClientEntry.grid(column= 0, row = 10, sticky = "nwn")
+borderEntryCep = Frame(signUpFrame, bg = "#85D3FF")
+borderEntryCep.grid(column= 0, row = 10, sticky = "nwn")
+nCepClientEntry = Entry(borderEntryCep)
+nCepClientEntry.grid(column = 0, row = 0, padx = (1.5, 0))
 
 nHoodClient = Label(signUpFrame, text = "Bairro")
 nHoodClient.grid(column = 0, row = 12, sticky = "w")
-nHoodClientEntry = Entry(signUpFrame)
-nHoodClientEntry.grid(column = 0, row = 13, sticky = "we")
+borderEntryNHood = Frame(signUpFrame, bg = "#85D3FF")
+borderEntryNHood.grid(column = 0, row = 13, sticky = "we")
+nHoodClientEntry = Entry(borderEntryNHood)
+nHoodClientEntry.grid(column = 0, row = 0, padx = (1.5, 0))
 
 nameClient = Label(signUpFrame, text = "Nome")
 nameClient.grid(column = 1, row = 0, sticky = "w", padx = 20)
@@ -137,19 +147,19 @@ ufClientEntry = Entry(signUpFrame)
 ufClientEntry.grid(column = 2, row = 13, sticky = "w")
 
 # Creating Blank Spaces Between Rows
-firstBlankSpace = Label(signUpFrame, text = "", bg = "#ff0000")
+firstBlankSpace = Label(signUpFrame, text = "", bg = "#FFF")
 firstBlankSpace.grid(column = 0, row = 2, sticky = "nsew", columnspan = 4)
 
-secondBlankSpace = Label(signUpFrame, text = "", bg = "#ff0000")
+secondBlankSpace = Label(signUpFrame, text = "", bg = "#FFF")
 secondBlankSpace.grid(column = 0, row = 5, sticky = "nsew", columnspan = 4)
 
-thirdBlankSpace = Label(signUpFrame, text = "", bg = "#ff0000")
+thirdBlankSpace = Label(signUpFrame, text = "", bg = "#FFF")
 thirdBlankSpace.grid(column = 0, row = 8, sticky = "nsew", columnspan = 4)
 
-fourthBlankSpace = Label(signUpFrame, text = "", bg = "#ff0000")
+fourthBlankSpace = Label(signUpFrame, text = "", bg = "#FFF")
 fourthBlankSpace.grid(column = 0, row = 11, sticky = "nsew", columnspan = 4)
 
-buttonWrapper = Frame(aTestingFrame, bg = "#000")
+buttonWrapper = Frame(aTestingFrame, bg = "#FFF")
 buttonWrapper.place(relx = .5, rely = .85, anchor = "s")
 
 buttonWrapper.grid_columnconfigure(0, weight = 1)
@@ -157,13 +167,16 @@ buttonWrapper.grid_columnconfigure(1, weight = 1)
 buttonWrapper.grid_columnconfigure(2, weight = 1)
 buttonWrapper.grid_rowconfigure(0, weight = 1)
 
-saveButton = Button(buttonWrapper, text = "Salvar", padx = 35)
+saveButton = Button(buttonWrapper, text = "Salvar", padx = 35, activebackground = "#76BCE3", bg = "#85D3FF", bd = 0)
 saveButton.grid(column = 0, row = 0, padx = 50)
 
-updateButton = Button(buttonWrapper, text = "Alterar", padx = 30)
-updateButton.grid(column = 1, row = 0, padx = 50)
+borderButton = Frame(buttonWrapper, bg = "#85D3FF", padx = 1.5, pady = 1.5)
+borderButton.grid(column = 1, row = 0, padx = 50)
 
-deleteButton = Button(buttonWrapper, text = "Excluir", padx = 25)
+updateButton = Button(borderButton, text = "Alterar", padx = 30, activebackground = "#FFF", bd = 0, bg = "#FFF")
+updateButton.pack()
+
+deleteButton = Button(buttonWrapper, text = "Excluir", padx = 25, activebackground = "#C5C5C5", activeforeground = "#777", bd = 0, bg = "#D9D9D9", fg = "#777")
 deleteButton.grid(column = 2, row = 0, padx = 50)
 
 signUpOwner.mainloop()

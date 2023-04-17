44b2bbfe9023403760b726b2bb62f1b60b157b79
unknown <amnd.castro@gmail.com>
2023-04-17 10:16:45 -0300

    tudo ok, falta testar

--- agenda.py
+++ agenda.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import subprocess
+# Importação da biblioteca para edeição de imagens
 from PIL import Image, ImageTk
 PC = Tk()
+
+def ir_menu():
+    subprocess.run(["python", "__menu__.py"])
+    PC.quit()
+
+def agendamento():
+    subprocess.run(["python", "agendamento.py"])
+
+def owner():
+    subprocess.run(["python", "__signUpOwner__.py"])
 
 # Configurações da tela
 PC.title("Pesquisa Cliente Dogin's")
@@ -25,50 +37,44 @@ PC.configure(bg='#fff')
 
 #foto "logo"
 fotoOriginal = Image.open("images/logo.png")
-fotoResize = fotoOriginal.resize((150, 30))
+fotoResize = fotoOriginal.resize((160, 40))
 fotoLogo = ImageTk.PhotoImage(fotoResize)
-#Label para guardar a imagem Logo
 test = Label(PC, bg="#FFFFFF", image=fotoLogo).place(x=80, y=30)
 
 # escrita "Agendamento"
 lbl_agendamento = Label(PC, text="Agendamento", bg="#FFF", font=(
-    "Helvetica 12 bold")).place(relx=.50, rely=.200, anchor="center")
+    "Helvetica 14 bold")).place(relx=.50, rely=.200, anchor="center")
 # coracao
 fotoOriginal = Image.open("images/coracao.png")
 fotoResize = fotoOriginal.resize((15, 13))
 fotoCoracao = ImageTk.PhotoImage(fotoResize)
 coracao = Label(PC, bg="#FFF", image=fotoCoracao).place(
-    relx=.58, rely=.200, anchor="center")
+    relx=.59, rely=.200, anchor="center")
 
-# botao voltar ao menu
-btn_menu = Button(PC, text="Voltar ao menu", bd=0, bg="#FFF", fg="#777777", font="Helvetica 10 underline", activebackground="#FFF", activeforeground="#777")
+#caixa de texto
+btn_menu = Button(PC, text="Voltar ao menu", bd=0, bg="#FFF", fg="#777777", font="Helvetica 10 underline", activebackground="#FFF", activeforeground="#777", command=ir_menu)
 btn_menu.place(x=750, y=30, anchor="n")
 
-
-# escrita "Nome do Cliente"
-lbl_nome = Label(PC, bg="#FFF", text="Nome do Cliente",
-                    font="Helvetica 10").place(x=200, y=180)
-# caixa de texto "nome do cliente"
+lbl_nome = Label(PC, bg="#FFF", text="Nome do Cliente",font="Helvetica 10").place(x=200, y=180)
 text_nome = Entry(PC, width=40).place(x=310, y=180)
 
 # foto "table"
 fotoOriginal = Image.open("images/table.png")
 fotoResize = fotoOriginal.resize((700, 225))
 fotoTable = ImageTk.PhotoImage(fotoResize)
-
 table = Label(PC, bg="#FFF", image=fotoTable).place(x=90, y=230)
 
 # botao "Agendar" e "Editar Cliente"
-btn_agendar = Button(PC, width=15, text="Agendar", activebackground="#76bce3", bd=0, bg="#85D3FF",compound=TOP).place(x=330, y=470)
-btn_editar = Button(PC, width=15, text="Editar Cliente", bg="#fff", bd=1,compound=TOP).place(x=450, y=470)
+btn_agendar = Button(PC, width=15, text="Agendar", activebackground="#76bce3", bd=1, bg="#85D3FF",compound=TOP, command=agendamento).place(x=330, y=470)
+btn_editar = Button(PC, width=15, text="Editar Cliente", bg="#D9D9D9", bd=1,compound=TOP, command=owner).place(x=450, y=470)
 
-# escrita "Não encontrou"
+#text "Não encontrou"
 lbl_voltar = Label(PC, bg="#FFF", text="Não encontrou o que procurava? Cadastre um novo cliente:",
     font="Helvetica 10").place(x=400, y=520, anchor="center")
 
-# botao cadastrar clienete
+#botao cadastrar clienete
 btn_cadastrar = Button(PC, text="Cadastrar Cliente", bd=0, bg="#FFF", fg="#777777",
-                  font="Helvetica 10 underline", activebackground="#FFF", activeforeground="#777")
+                  font="Helvetica 10 underline", activebackground="#FFF", activeforeground="#777", command=owner)
 btn_cadastrar.place(x=625, y=508, anchor="n")
 
 PC.mainloop()
--- agendamento.py
+++ agendamento.py
@@ -1,8 +1,16 @@
 from tkinter import *
-
-# Importação da biblioteca para edeição de imagens
 from PIL import Image, ImageTk
+from tkinter import ttk
+import subprocess
 agendamento = Tk()
+
+def ir_menu():
+    subprocess.run(["python", "__menu__.py"])
+    agendamento.quit()
+
+def success():
+    subprocess.run(["python", "success.py"])
+    agendamento.quit()
 
 # Configurações da tela
 agendamento.title("Pesquisa Cliente Dogin's")
@@ -13,7 +21,7 @@ width_screen = agendamento.winfo_screenwidth()
 height_screen = agendamento.winfo_screenheight()
 
 width = 870
-height = 570
+height = 520
 
 posx = (width_screen / 2) - (width / 2)
 posy = (height_screen / 2) - (height / 2)
@@ -26,15 +34,21 @@ agendamento.configure(bg='#fff')
 
 # foto "logo" e Label para guardar a imagem Logo
 fotoOriginal = Image.open("images/logo.png")
-fotoResize = fotoOriginal.resize((150, 30))
+fotoResize = fotoOriginal.resize((160, 40))
 fotoLogo = ImageTk.PhotoImage(fotoResize)
-test = Label(agendamento, bg="#FFFFFF", image=fotoLogo).place(x=80, y=30)
+logo = Label(agendamento, bg="#FFFFFF", image=fotoLogo).place(x=80, y=30)
 
 # foto "barra de progresso" e Label para "barra de progresso"
 fotoOriginal = Image.open("images/progresso.png")
 fotoResize = fotoOriginal.resize((180, 60))
 fotoProgresso = ImageTk.PhotoImage(fotoResize)
-test = Label(agendamento, bg="#FFFFFF", image=fotoProgresso).place(relx=.50, rely=.100, anchor="center")
+progresso = Label(agendamento, bg="#FFFFFF", image=fotoProgresso).place(
+    relx=.50, rely=.100, anchor="center")
+
+# botao voltar ao menu
+btn_menu = Button(agendamento, text="Voltar ao menu", bd=0, bg="#FFF", fg="#777777",
+                  font="Helvetica 10 underline", activebackground="#FFF", activeforeground="#777", command=ir_menu)
+btn_menu.place(x=750, y=30, anchor="n")
 
 # escrita "Agendamento"
 lbl_agendamento = Label(agendamento, text="Agendamento", bg="#FFF", font=(
@@ -43,36 +57,44 @@ lbl_agendamento = Label(agendamento, text="Agendamento", bg="#FFF", font=(
 fotoOriginal = Image.open("images/coracao.png")
 fotoResize = fotoOriginal.resize((15, 13))
 fotoCoracao = ImageTk.PhotoImage(fotoResize)
-coracao = Label(agendamento, bg="#FFF", image=fotoCoracao).place(relx=.60, rely=.200, anchor="e")
+coracao = Label(agendamento, bg="#FFF", image=fotoCoracao).place(
+    relx=.60, rely=.200, anchor="e")
 
-#text "Nome do Dono " e caixa de texto "Nome do dono"
-lbl_nome = Label(agendamento, bg="#FFF", text="Nome do dono",font="Helvetica 10").place(x=200, y=150)
+# caixas de texto e de seleção
+lbl_nome = Label(agendamento, bg="#FFF", text="Nome dono",
+                 font="Helvetica 10").place(x=200, y=150)
 text_nome = Entry(agendamento, width=30).place(x=200, y=180)
 
-#text "Escolha o pet" e text "Escolha o pet" e 
-lbl_pet = Label(agendamento, bg="#FFF", text="Escolha o pet",font="Helvetica 10").place(x=200, y=220)
-text_pet = Entry(agendamento, width=30).place(x=200, y=250)
+lbl_pet = Label(agendamento, bg="#FFF", text="Escolha o pet",
+                font="Helvetica 10").place(x=200, y=220)
+txt_especie = ttk.Combobox(
+    agendamento, values=["Alfredo", "Totó"], width=30).place(x=200, y=250)
 
-# text "Data" e vcaixa de texto "Data"
-lbl_data = Label(agendamento, bg="#FFF", text="Data",font="Helvetica 10").place(x=200, y=300)
+lbl_data = Label(agendamento, bg="#FFF", text="Data",
+                 font="Helvetica 10").place(x=200, y=300)
 text_data = Entry(agendamento, width=15).place(x=200, y=330)
 
-# text "Hora" e caixa de texto "UF"
-lbl_hora = Label(agendamento, bg="#FFF", text="Hora",font="Helvetica 10").place(x=370, y=300)
-text_hora = Entry(agendamento, width=10).place(x=370, y=330)
+lbl_hora = Label(agendamento, bg="#FFF", text="Hora",
+                 font="Helvetica 10").place(x=350, y=300)
+text_hora = Entry(agendamento, width=15).place(x=350, y=330)
 
-#Checkbutton's
+# Checkbutton's
+
+
 def vacinacaoClicado():
     esp = str(vvacinacao.get())
     print("Vacimação:"+esp)
+
 
 def banhoClicado():
     esp = str(vbanho.get())
     print("Banho:"+esp)
 
+
 def tosaClicado():
     esp = str(vtosa.get())
     print("Tosa:"+esp)
+
 
 def limpezaClicado():
     esp = str(vlimpeza.get())
@@ -84,26 +106,62 @@ vbanho = IntVar()
 vtosa = IntVar()
 vlimpeza = IntVar()
 
-fr_quadrado1 = Frame(agendamento, borderwidth=1, relief="groove",bg="#FFFFFF")
-fr_quadrado1.place(x=470, y=230, width=200, height=100)
+# text "Escolha um ou mais serviços"
+lbl_nome = Label(agendamento, bg="#FFF", text="Escolha um ou mais serviços",
+                 font="Helvetica 10").place(x=520, y=150)
 
-cb_vacinacao = Checkbutton(fr_quadrado1, text="Vacicao",variable=vacinacaoClicado,onvalue=0, offvalue="n",command=vacinacaoClicado)
-cb_vacinacao.pack(side=TOP)
+fr_quadrado1 = Frame(agendamento, borderwidth=0, relief="groove", bg="#FFFFFF")
+fr_quadrado1.place(x=520, y=170, width=200, height=170)
 
-cb_vbanho = Checkbutton(fr_quadrado1, text="Banho",bg="#FFFFFF",variable=banhoClicado,onvalue=0, offvalue="n",command=vacinacaoClicado)
-cb_vbanho.pack(side=TOP)
+cb_vacinacao = Checkbutton(fr_quadrado1, text="Vacinação", font="Helvetica 10", bg="#FFFFFF",
+                           variable=vacinacaoClicado, onvalue=0, offvalue="n", command=vacinacaoClicado)
+cb_vacinacao.place(x=10, y=10)
+lbl_vacinacao = Label(agendamento, bg="#FFF", text="R$ 00,00",
+                      fg="#777777", font="Helvetica 9").place(x=620, y=182)
 
-cb_vtosa = Checkbutton(fr_quadrado1, text="Tosa",bg="#FFFFFF",variable=tosaClicado,onvalue=0, offvalue="n",command=vacinacaoClicado)
-cb_vtosa.pack(side=TOP)
+cb_vbanho = Checkbutton(fr_quadrado1, text="Banho", font="Helvetica 10", bg="#FFFFFF",
+                        variable=banhoClicado, onvalue=0, offvalue="n", command=vacinacaoClicado)
+cb_vbanho.place(x=10, y=40)
+lbl_banho = Label(agendamento, bg="#FFF", text="R$ 00,00",
+                  fg="#777777", font="Helvetica 9").place(x=600, y=212)
 
-cb_limpeza = Checkbutton(fr_quadrado1, bg="#FFFFFF",text="Limpeza Dentária",variable=limpezaClicado,onvalue=0, offvalue="n",command=vacinacaoClicado)
-cb_limpeza.pack(side=TOP)
+cb_vtosa = Checkbutton(fr_quadrado1, text="Tosa", font="Helvetica 10", bg="#FFFFFF",
+                       variable=tosaClicado, onvalue=0, offvalue="n", command=vacinacaoClicado)
+cb_vtosa.place(x=10, y=70)
+lbl_tosa = Label(agendamento, bg="#FFF", text="R$ 00,00",
+                 fg="#777777", font="Helvetica 9").place(x=590, y=242)
 
-#botao "Salvar", "Alterar", "Excluir"
-btn_salvar = Button(agendamento, width=15, text="Salvar", activebackground="#76bce3", bd=0, bg="#85D3FF",compound=TOP).place(x=250, y=450)
-btn_alterar = Button(agendamento, width=15, text="Alterar Cliente", bg="#fff", bd=1,compound=TOP).place(x=380, y=450)
-btn_excluir = Button(agendamento, width=15, text="Excluir Cliente", bg="#fff", bd=1,compound=TOP).place(x=510, y=450)
+cb_limpeza = Checkbutton(fr_quadrado1, font="Helvetica 10", bg="#FFFFFF", text="Limpeza Dentária",
+                         variable=limpezaClicado, onvalue=0, offvalue="n", command=vacinacaoClicado)
+cb_limpeza.place(x=10, y=100)
+lbl_limpeza = Label(agendamento, bg="#FFF", text="R$ 00,00",
+                    fg="#777777", font="Helvetica 9").place(x=660, y=272)
 
+# text "Total"
+lbl_total = Label(agendamento, bg="#FFF", text="Total",
+                  font="Helvetica 10").place(x=530, y=300)
+lbl_total = Label(agendamento, bg="#FFF", text="R$ 00,00",
+                  fg="#777777", font="Helvetica 10 underline").place(x=570, y=302)
+
+#img calendario
+fotoOriginal = Image.open("images/calendar.png")
+fotoResize = fotoOriginal.resize((15, 17))
+fotoData = ImageTk.PhotoImage(fotoResize)
+Data = Label(agendamento,  image=fotoData).place(x=277, y=326)
+
+#img hora
+fotoOriginal = Image.open("images/hora.png")
+fotoResize = fotoOriginal.resize((15, 16))
+fotoHora = ImageTk.PhotoImage(fotoResize)
+Hora = Label(agendamento,image=fotoHora).place(x=424, y=326)
+
+# botao "Salvar", "Alterar", "Excluir"
+btn_salvar = Button(agendamento, width=15, text="Salvar", activebackground="#76bce3",
+                    bd=1, bg="#85D3FF", compound=TOP, command=success).place(x=250, y=450)
+btn_excluir = Button(agendamento, width=15, text="Excluir",
+                     bg="#D9D9D9", bd=1, compound=TOP, command=success).place(x=380, y=450)
+btn_alterar = Button(agendamento, width=15, text="Alterar",
+                     bg="#D9D9D9", bd=0, compound=TOP, command=success).place(x=510, y=450)
 
 
 agendamento.mainloop()
